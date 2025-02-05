99953182dd788c94d9072297b0f9b860a09c3f4f
Loknath Dhar <loknath-dhar@pm.me>
2025-02-05 20:00:34 +0600

    refactor olde code and start new

--- normalized_url.go
+++ normalized_url.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 )
 
 func normalizeURL(address string) (string, error) {
 	urlStr, err := url.Parse(address)
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
+	}
 
-    trimmedPath := strings.TrimSuffix(urlStr.Path, "/")
+	trimmedPath := strings.TrimSuffix(urlStr.Path, "/")
 
-    normStr := urlStr.Host + trimmedPath
-    return normStr,nil
+	normStr := urlStr.Host + trimmedPath
+	return strings.ToLower(normStr), nil
+}
+
+func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	return nil, nil
 }
--- normalized_url_test.go
+++ normalized_url_test.go
@@ -1,45 +1,113 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
-		name     string
-		inputURL string
-		expected string
+		name          string
+		inputURL      string
+		expected      string
+		errorContains string
 	}{
 		{
-			name: "remove scheme",
+			name:     "remove scheme",
 			inputURL: "https://blog.boot.dev/path",
 			expected: "blog.boot.dev/path",
 		},
 		{
-			name: "remove http scheme",
+			name:     "remove http scheme",
 			inputURL: "http://blog.boot.dev/path",
 			expected: "blog.boot.dev/path",
 		},
 		{
-			name: "remove the https scheme with trailing slash",
+			name:     "remove the https scheme with trailing slash",
 			inputURL: "https://blog.boot.dev/path/",
 			expected: "blog.boot.dev/path",
 		},
 		{
-			name: "remove the http scheme with trailing slash",
+			name:     "remove the http scheme with trailing slash",
 			inputURL: "http://blog.boot.dev/path/",
 			expected: "blog.boot.dev/path",
+		},
+		{
+			name:     "lowercase capital letters",
+			inputURL: "https://blog.BOOT.DEV/pATH",
+			expected: "blog.boot.dev/path",
+		},
+		{
+			name:          "handler invalid URL",
+			inputURL:      "://invalidURL",
+			expected:      "",
+			errorContains: "couldn't parse URL",
 		},
 	}
 
 	for i, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			actual, err := normalizeURL(tc.inputURL)
-			if err != nil {
-				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
+				assertErrorMessage(t, i, tc.name, err)
 				return
+			} else if err != nil && tc.errorContains == "" {
+				assertErrorMessage(t, i, tc.name, err)
+				return
+			} else if err == nil && tc.errorContains != "" {
+				t.Errorf("Test %v - '%s' FAIL: expected error containing '%v', got none", i, tc.name, tc.errorContains)
 			}
+
 			if actual != tc.expected {
 				t.Errorf("Test %v - %s FAIL: \nexpected URL: %v, \nactual: %v", i, tc.name, tc.expected, actual)
 			}
 		})
 	}
+}
+
+func TestGetURLsFromHTML(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputURL  string
+		inputBody string
+		expected  []string
+	}{
+		{
+			name:     "absolute and relative paths",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+<html>
+	<body>
+		<a href="/path/one">
+			<span>Boot.dev</span>
+		</a>
+		<a href="https://other.com/path/one">
+			<span>Boot.dev</span>
+		</a>
+	</body>
+</html>
+			`,
+			expected: []string{"https://blog.boot.dev/path/one", "https://other.com/path.one"},
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := getURLsFromHTML(tc.inputBody, tc.inputURL)
+			if err != nil {
+				assertErrorMessage(t, i, tc.name, err)
+				return
+			}
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Test %v - '%s' FAIL: \nexpected: %v, \nactual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
+
+func assertErrorMessage(t testing.TB, i int, name string, err error) {
+	t.Helper()
+	t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, name, err)
 }
