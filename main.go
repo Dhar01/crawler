74e0f326a8cd4c6a3ba581a05b7bd99115fb3856
Loknath Dhar <loknath-dhar@pm.me>
2025-03-10 16:25:49 +0600

    taking command line arguments

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} else {
+		fmt.Printf("starting crawl of: %s\n", args[1])
+	}
+
+	website := args[1]
+
+	getHTML(website)
 }
